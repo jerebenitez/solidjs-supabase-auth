c538b4b69833aa7a641cf30e2fca1e9a1a6381c2
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-04-03 19:16:58 -0300

    Improved button component, still WIP

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -5,6 +5,6 @@ export default function Counter() {
   const [count, setCount] = createSignal(0);
   
   return (
-    <Button onClick={() => setCount(count() + 1)}>Clicks: {count()}</Button>
+    <Button variant="outline" onClick={() => setCount(count() + 1)}>Clicks: {count()}</Button>
   )
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,21 +1,48 @@
-import { children } from "solid-js"
+import { children, JSX } from "solid-js"
+import { twMerge } from "tailwind-merge";
 
-type BUttonProps = {
-    children: HTMLElement,
-    variant?: "default" | "alternative" | "dark" | "light"
-    color?: string
+type ButtonProps = {
+    children: JSX.Element | JSX.Element[] | string,
+    variant?: "primary" | "secondary" | "success" | "danger" | "warning" | "dark" | "light" | "outline" 
+    size?: "sm" | "md" | "lg" | "icon"
+    pill?: boolean
 }
 
-export const Button = (props: BUttonProps) => {
+export const Button = (props: ButtonProps & JSX.ButtonHTMLAttributes<HTMLButtonElement>) => {
     const safeChildren = children(() => props.children)
 
+    const baseStyles = "text-white font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none focus:ring-4 transition";
+    const variants = {
+        primary: "bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 disabled:bg-blue-400 dark:disabled:bg-blue-500 disabled:cursor-not-allowed",
+        secondary: "bg-gray-700 hover:bg-gray-800 focus:ring-gray-300 disabled:bg-gray-400 dark:disabled:bg-gray-500 disabled:cursor-not-allowed",
+        success: "bg-green-700 hover:bg-green-800 focus:ring-green-300 disabled:bg-green-400 dark:disabled:bg-green-500 disabled:cursor-not-allowed",
+        danger: "bg-red-700 hover:bg-red-800 focus:ring-red-300 disabled:bg-red-400 dark:disabled:bg-red-500 disabled:cursor-not-allowed",
+        warning: "bg-yellow-500 hover:bg-yellow-600 focus:ring-yellow-300 disabled:bg-yellow-400 dark:disabled:bg-yellow-500 disabled:cursor-not-allowed",
+        dark: "bg-gray-900 hover:bg-black focus:ring-gray-700 disabled:bg-gray-600 dark:disabled:bg-gray-700 disabled:cursor-not-allowed",
+        light: "bg-gray-200 text-gray-900 hover:bg-gray-300 focus:ring-gray-400 disabled:bg-gray-300 dark:disabled:bg-gray-400 disabled:cursor-not-allowed",
+        outline: "border border-gray-300 text-gray-900 hover:bg-gray-100 focus:ring-gray-300 disabled:border-gray-400 disabled:text-gray-500 disabled:bg-gray-100 disabled:cursor-not-allowed",
+      };
+  
+    const sizes = {
+      sm: "text-xs px-3 py-2",
+      md: "text-sm px-5 py-2.5",
+      lg: "text-lg px-6 py-3",
+      icon: ""
+    };
+
+    const classes = twMerge(
+        baseStyles,
+        props.variant ? variants[props.variant] : variants.primary,
+        props.size ? sizes[props.size] : sizes.md,
+        props.class
+    )
+
     return (
-        <button 
-          type="button"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          {...props}
+        <button
+            class={classes}
+            {...props}
         >
             {safeChildren()}
         </button>
-    )
-}
+    );
+};
