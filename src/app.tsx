474c498ed57ffa1f5ddad983470db307b41618ed
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-04-02 23:12:56 -0300

    chore: added solid meta

--- src/app.tsx
+++ src/app.tsx
@@ -3,12 +3,16 @@ import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import Nav from "~/components/Nav";
 import "./app.css";
+import { MetaProvider, Title } from "@solidjs/meta";
 
 export default function App() {
   return (
     <Router
       root={props => (
         <>
+          <MetaProvider>
+            <Title>SolidJs Auth</Title>
+          </MetaProvider>
           <Nav />
           <Suspense>{props.children}</Suspense>
         </>
