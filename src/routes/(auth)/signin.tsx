deba13989e6bb51fabe9c64af1c56558570177cf
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-04-04 11:16:36 -0300

    feat: Separator component

--- src/routes/(auth)/signin.tsx
+++ src/routes/(auth)/signin.tsx
@@ -1,5 +1,6 @@
 import { Button } from "~/components/ui/Button";
 import { Card, CardTitle } from "~/components/ui/Card";
+import { Separator } from "~/components/ui/Separator";
 
 export default function SignIn() {
     return (
@@ -23,11 +24,8 @@ export default function SignIn() {
             </div>
             <a href="#" class="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
         </div>
-        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in to your account</button>
-        <div class="inline-flex items-center justify-center w-full">
-            <hr class="w-64 h-px my-4" />
-            <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 left-1/2 dark:text-white dark:bg-gray-800">or</span>
-        </div>
+        <Button type="submit" class="w-full">Sing in to your account</Button>
+        <Separator label="or" />
         <Button variant="secondary" class="flex items-center w-full justify-center">
             <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
                 <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd"/>
