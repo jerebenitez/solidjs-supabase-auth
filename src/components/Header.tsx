acdcf80ccfb3a46911b5c64665d126293118d781
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-04-11 21:35:04 -0300

    feat: project dropdown WIP

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
 import { initFlowbite } from 'flowbite'
 import { onMount } from 'solid-js'
+import { DropdownOptions, RadioDropdown } from './ui/RadioDropdown'
 
 export function Header() {
+    const projects: DropdownOptions[] = [
+        { title: "Individual", description: "Some helpful instruction goes over here." },
+        { title: "Company", description: "Some helpful instruction goes over here." },
+        { title: "Non profit", description: "Some helpful instruction goes over here." },
+    ]
+
     onMount(() => {
         initFlowbite()
     })
@@ -67,124 +74,10 @@ export function Header() {
                             </span>
                         </a>
 
-                        <button
-                            id="dropdownRadioHelperButton"
-                            data-dropdown-toggle="dropdownRadioHelper"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                            type="button"
-                        >
-                            Project Name
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                class="ml-2 lucide lucide-chevrons-up-down-icon lucide-chevrons-up-down"
-                            >
-                                <path d="m7 15 5 5 5-5" />
-                                <path d="m7 9 5-5 5 5" />
-                            </svg>
-                        </button>
-
-                        <div
-                            id="dropdownRadioHelper"
-                            class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-60 dark:bg-gray-700 dark:divide-gray-600"
-                        >
-                            <ul
-                                class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200"
-                                aria-labelledby="dropdownRadioHelperButton"
-                            >
-                                <li>
-                                    <div class="flex p-2 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600">
-                                        <div class="flex items-center h-5">
-                                            <input
-                                                id="helper-radio-4"
-                                                name="helper-radio"
-                                                type="radio"
-                                                value=""
-                                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                                            />
-                                        </div>
-                                        <div class="ms-2 text-sm">
-                                            <label
-                                                for="helper-radio-4"
-                                                class="font-medium text-gray-900 dark:text-gray-300"
-                                            >
-                                                <div>Individual</div>
-                                                <p
-                                                    id="helper-radio-text-4"
-                                                    class="text-xs font-normal text-gray-500 dark:text-gray-300"
-                                                >
-                                                    Some helpful instruction
-                                                    goes over here.
-                                                </p>
-                                            </label>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="flex p-2 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600">
-                                        <div class="flex items-center h-5">
-                                            <input
-                                                id="helper-radio-5"
-                                                name="helper-radio"
-                                                type="radio"
-                                                value=""
-                                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                                            />
-                                        </div>
-                                        <div class="ms-2 text-sm">
-                                            <label
-                                                for="helper-radio-5"
-                                                class="font-medium text-gray-900 dark:text-gray-300"
-                                            >
-                                                <div>Company</div>
-                                                <p
-                                                    id="helper-radio-text-5"
-                                                    class="text-xs font-normal text-gray-500 dark:text-gray-300"
-                                                >
-                                                    Some helpful instruction
-                                                    goes over here.
-                                                </p>
-                                            </label>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="flex p-2 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600">
-                                        <div class="flex items-center h-5">
-                                            <input
-                                                id="helper-radio-6"
-                                                name="helper-radio"
-                                                type="radio"
-                                                value=""
-                                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                                            />
-                                        </div>
-                                        <div class="ms-2 text-sm">
-                                            <label
-                                                for="helper-radio-6"
-                                                class="font-medium text-gray-900 dark:text-gray-300"
-                                            >
-                                                <div>Non profit</div>
-                                                <p
-                                                    id="helper-radio-text-6"
-                                                    class="text-xs font-normal text-gray-500 dark:text-gray-300"
-                                                >
-                                                    Some helpful instruction
-                                                    goes over here.
-                                                </p>
-                                            </label>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
+                    <RadioDropdown 
+                        title="Project Name" 
+                        options={projects}
+                    />
 
                     </div>
                     <div class="flex items-center lg:order-2 gap-4">
---
+++ src/components/ui/RadioDropdown.tsx
@@ -0,0 +1,72 @@
+import { initFlowbite } from 'flowbite'
+import { onMount } from 'solid-js'
+
+export type DropdownOptions = {
+    title: string
+    description?: string
+}
+
+type DropdownProps = {
+    title: string
+    icon?: string
+    options: DropdownOptions[]
+}
+
+export function RadioDropdown(props: DropdownProps) {
+    onMount(() => {
+        initFlowbite()
+    })
+
+    return (
+        <>
+            <button
+                id="dropdownRadioHelperButton"
+                data-dropdown-toggle="dropdownRadioHelper"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                type="button"
+            >
+                {props.title}
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevrons-up-down-icon lucide-chevrons-up-down"><path d="m7 15 5 5 5-5"/><path d="m7 9 5-5 5 5"/></svg>
+            </button>
+            <div
+                id="dropdownRadioHelper"
+                class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-60 dark:bg-gray-700 dark:divide-gray-600"
+            >
+                <ul
+                    class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200"
+                    aria-labelledby="dropdownRadioHelperButton"
+                >
+                {props.options.map(option => (
+                    <li>
+                        <div class="flex p-2 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600">
+                            <div class="flex items-center h-5">
+                                <input
+                                    id="helper-radio-4"
+                                    name="helper-radio"
+                                    type="radio"
+                                    value=""
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                                />
+                            </div>
+                            <div class="ms-2 text-sm">
+                                <label
+                                    for="helper-radio-4"
+                                    class="font-medium text-gray-900 dark:text-gray-300"
+                                >
+                                    <div>{option.title}</div>
+                                    {option.description && (
+                                    <p
+                                        id="helper-radio-text-4"
+                                        class="text-xs font-normal text-gray-500 dark:text-gray-300"
+                                    >{option.description}</p>
+                                    )}
+                                </label>
+                            </div>
+                        </div>
+                    </li>
+                ))}
+                </ul>
+            </div>
+        </>
+    )
+}
