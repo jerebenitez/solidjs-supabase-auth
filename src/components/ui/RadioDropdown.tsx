39dea92a4f57e650f070885e072cb48098788c6d
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-04-11 21:50:59 -0300

    feat: added lucide icons

--- src/components/ui/RadioDropdown.tsx
+++ src/components/ui/RadioDropdown.tsx
@@ -1,6 +1,7 @@
 import { initFlowbite } from 'flowbite'
 import { onMount } from 'solid-js'
 import { Button } from './Button'
+import ChevronsUpDown from 'lucide-solid/icons/chevrons-up-down'
 
 export type DropdownOptions = {
     id: number | string | string[]
@@ -23,13 +24,14 @@ export function RadioDropdown(props: DropdownProps) {
     return (
         <>
             <Button
+                variant='secondary'
                 id="dropdownRadioHelperButton"
                 data-dropdown-toggle="dropdownRadioHelper"
                 class="inline-flex items-center gap-2"
                 type="button"
             >
                 {props.title}
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevrons-up-down-icon lucide-chevrons-up-down"><path d="m7 15 5 5 5-5"/><path d="m7 9 5-5 5 5"/></svg>
+                <ChevronsUpDown />
             </Button>
             <div
                 id="dropdownRadioHelper"
