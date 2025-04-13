5897c0347a1f0ac48cc7d6064d475e5eb0af22e6
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-04-12 22:18:11 -0300

    feat: label v1

---
+++ src/components/ui/Label.tsx
@@ -0,0 +1,20 @@
+import { JSX, splitProps } from "solid-js";
+import { cn } from "~/lib/utils";
+
+export function Label(props: JSX.LabelHTMLAttributes<HTMLLabelElement>) {
+    const [classes, restProps] = splitProps(
+        props,
+        ['class']
+    )
+
+    return (
+        <label 
+            {...restProps}
+            class={cn(
+                "block mb-2 text-sm font-medium text-gray-900 dark:text-white",
+                classes
+            )}>
+            {props.children}
+        </label>
+    )
+}
--- src/routes/(dashboard)/profile.tsx
+++ src/routes/(dashboard)/profile.tsx
@@ -1,164 +1,110 @@
-import { Button } from "~/components/ui/Button";
-import { Input } from "~/components/ui/Input";
+import { Button } from '~/components/ui/Button'
+import { Input } from '~/components/ui/Input'
+import { Label } from '~/components/ui/Label'
 
 export default function ProfilePage() {
     return (
-        <>
-            <form>
-                <div class="grid gap-6 mb-6 md:grid-cols-2">
-                    <div>
-                        <label
-                            for="first_name"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                        >
-                            First name
-                        </label>
-                        <Input
-                            type="text"
-                            id="first_name"
-                            placeholder="John"
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label
-                            for="last_name"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                        >
-                            Last name
-                        </label>
-                        <Input
-                            type="text"
-                            id="last_name"
-                            placeholder="Doe"
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label
-                            for="company"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                        >
-                            Company
-                        </label>
-                        <Input
-                            type="text"
-                            id="company"
-                            placeholder="Flowbite"
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label
-                            for="phone"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                        >
-                            Phone number
-                        </label>
-                        <Input
-                            type="tel"
-                            id="phone"
-                            placeholder="123-45-678"
-                            pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label
-                            for="website"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                        >
-                            Website URL
-                        </label>
-                        <Input
-                            type="url"
-                            id="website"
-                            placeholder="flowbite.com"
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label
-                            for="visitors"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                        >
-                            Unique visitors (per month)
-                        </label>
-                        <Input
-                            type="number"
-                            id="visitors"
-                            placeholder=""
-                            required
-                        />
-                    </div>
-                </div>
-                <div class="mb-6">
-                    <label
-                        for="email"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        Email address
-                    </label>
+        <form>
+            <div class="grid gap-6 mb-6 md:grid-cols-2">
+                <div>
+                    <Label for="first_name">First name</Label>
                     <Input
-                        type="email"
-                        id="email"
-                        placeholder="john.doe@company.com"
+                        type="text"
+                        id="first_name"
+                        placeholder="John"
                         required
                     />
                 </div>
-                <div class="mb-6">
-                    <label
-                        for="password"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        Password
-                    </label>
+                <div>
+                    <Label for="last_name">Last name</Label>
                     <Input
-                        type="password"
-                        id="password"
-                        placeholder="•••••••••"
+                        type="text"
+                        id="last_name"
+                        placeholder="Doe"
                         required
                     />
                 </div>
-                <div class="mb-6">
-                    <label
-                        for="confirm_password"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        Confirm password
-                    </label>
+                <div>
+                    <Label for="company">Company</Label>
                     <Input
-                        type="password"
-                        id="confirm_password"
-                        placeholder="•••••••••"
+                        type="text"
+                        id="company"
+                        placeholder="Flowbite"
                         required
                     />
                 </div>
-                <div class="flex items-start mb-6">
-                    <div class="flex items-center h-5">
-                        <Input
-                            id="remember"
-                            type="checkbox"
-                            value=""
-                            required
-                        />
-                    </div>
-                    <label
-                        for="remember"
-                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                    >
-                        I agree with the{' '}
-                        <a
-                            href="#"
-                            class="text-blue-600 hover:underline dark:text-blue-500"
-                        >
-                            terms and conditions
-                        </a>
-                        .
-                    </label>
+                <div>
+                    <Label for="phone">Phone number</Label>
+                    <Input
+                        type="tel"
+                        id="phone"
+                        placeholder="123-45-678"
+                        pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                        required
+                    />
                 </div>
-                <Button type="submit">Submit</Button>
-            </form>
-        </>
+                <div>
+                    <Label for="website">Website URL</Label>
+                    <Input
+                        type="url"
+                        id="website"
+                        placeholder="flowbite.com"
+                        required
+                    />
+                </div>
+                <div>
+                    <Label for="visitors">Unique visitors (per month)</Label>
+                    <Input
+                        type="number"
+                        id="visitors"
+                        placeholder=""
+                        required
+                    />
+                </div>
+            </div>
+            <div class="mb-6">
+                <Label for="email">Email address</Label>
+                <Input
+                    type="email"
+                    id="email"
+                    placeholder="john.doe@company.com"
+                    required
+                />
+            </div>
+            <div class="mb-6">
+                <Label for="password">Password</Label>
+                <Input
+                    type="password"
+                    id="password"
+                    placeholder="•••••••••"
+                    required
+                />
+            </div>
+            <div class="mb-6">
+                <Label for="confirm_password">Confirm password</Label>
+                <Input
+                    type="password"
+                    id="confirm_password"
+                    placeholder="•••••••••"
+                    required
+                />
+            </div>
+            <div class="flex items-start gap-2 mb-6">
+                <div class="flex items-center h-5">
+                    <Input id="remember" type="checkbox" value="" required />
+                </div>
+                <Label for="remember">
+                    I agree with the{' '}
+                    <a
+                        href="#"
+                        class="text-blue-600 hover:underline dark:text-blue-500"
+                    >
+                        terms and conditions
+                    </a>
+                    .
+                </Label>
+            </div>
+            <Button type="submit">Submit</Button>
+        </form>
     )
 }
