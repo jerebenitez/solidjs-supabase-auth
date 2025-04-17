eaa4bd71ce771f7a35e9949236a06d863d46d553
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-04-17 18:48:51 -0300

    refactor: use For in RadioDropdown

--- src/components/ui/RadioDropdown.tsx
+++ src/components/ui/RadioDropdown.tsx
@@ -1,5 +1,5 @@
 import { initFlowbite } from 'flowbite'
-import { onMount } from 'solid-js'
+import { For, onMount } from 'solid-js'
 import ChevronsUpDown from 'lucide-solid/icons/chevrons-up-down'
 import { DropdownTrigger } from './dropdown/DropdownTrigger'
 import { Dropdown } from './dropdown/Dropdown'
@@ -36,39 +36,44 @@ export function RadioDropdown(props: DropdownProps) {
                 <ChevronsUpDown />
             </DropdownTrigger>
             <Dropdown class="w-60" id="dropdownRadioHelper">
-                <DropdownContent class="p-3 space-y-1" aria-labelledby="dropdownRadioHelperButton">
-                    {props.options.map((option, idx) => (
-                        <li>
-                            <div class="flex p-2 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600">
-                                <div class="flex items-center h-5">
-                                    <input
-                                        id={`helper-radio-${idx}`}
-                                        name="helper-radio"
-                                        type="radio"
-                                        value={option.id}
-                                        checked={option.checked}
-                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                                    />
+                <DropdownContent
+                    class="p-3 space-y-1"
+                    aria-labelledby="dropdownRadioHelperButton"
+                >
+                    <For each={props.options}>
+                        {(option, idx) => (
+                            <li>
+                                <div class="flex p-2 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600">
+                                    <div class="flex items-center h-5">
+                                        <input
+                                            id={`helper-radio-${idx()}`}
+                                            name="helper-radio"
+                                            type="radio"
+                                            value={option.id}
+                                            checked={option.checked}
+                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                                        />
+                                    </div>
+                                    <div class="ms-2 text-sm">
+                                        <label
+                                            for={`helper-radio-${idx()}`}
+                                            class="font-medium text-gray-900 dark:text-gray-300"
+                                        >
+                                            <div>{option.title}</div>
+                                            {option.description && (
+                                                <p
+                                                    id={`helper-radio-${idx()}`}
+                                                    class="text-xs font-normal text-gray-500 dark:text-gray-300"
+                                                >
+                                                    {option.description}
+                                                </p>
+                                            )}
+                                        </label>
+                                    </div>
                                 </div>
-                                <div class="ms-2 text-sm">
-                                    <label
-                                        for={`helper-radio-${idx}`}
-                                        class="font-medium text-gray-900 dark:text-gray-300"
-                                    >
-                                        <div>{option.title}</div>
-                                        {option.description && (
-                                            <p
-                                                id={`helper-radio-${idx}`}
-                                                class="text-xs font-normal text-gray-500 dark:text-gray-300"
-                                            >
-                                                {option.description}
-                                            </p>
-                                        )}
-                                    </label>
-                                </div>
-                            </div>
-                        </li>
-                    ))}
+                            </li>
+                        )}
+                    </For>
                 </DropdownContent>
             </Dropdown>
         </>
