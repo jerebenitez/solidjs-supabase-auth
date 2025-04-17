cb969c45e93895d81cbc675922bf1b59c7f28a04
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-04-17 17:12:14 -0300

    feat: move data to file

---
+++ src/lib/data.ts
@@ -0,0 +1,20 @@
+import { DropdownOptions } from '~/components/ui/RadioDropdown'
+
+export const projects: DropdownOptions[] = [
+    {
+        id: 1,
+        title: 'Individual',
+        description: 'Some helpful instruction goes over here.',
+    },
+    {
+        id: 2,
+        title: 'Company',
+        description: 'Some helpful instruction goes over here.',
+    },
+    {
+        id: 3,
+        title: 'Non profit',
+        description: 'Some helpful instruction goes over here.',
+        checked: true,
+    },
+]
