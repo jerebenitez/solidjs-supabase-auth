3ab5f5907461398c7f32632c062b86ab6722e7ce
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-04-17 18:40:02 -0300

    feat: Separator now works without a label too

--- src/components/ui/Separator.tsx
+++ src/components/ui/Separator.tsx
@@ -1,14 +1,19 @@
+import { cn } from '~/lib/utils'
+
 type SeparatorProps = {
-    label: string
+    label?: string
+    class?: string
 }
 
 export function Separator(props: SeparatorProps) {
-    return (
+    return props.label ? (
         <div class="inline-flex items-center justify-center w-full">
-            <hr class="w-64 h-px my-4" />
+            <hr class={cn('w-64 h-px my-4', props.class)} />
             <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 left-1/2 dark:text-white dark:bg-gray-800">
                 {props.label}
             </span>
         </div>
+    ) : (
+        <hr class={cn('h-px dark:text-gray-600', props.class)} />
     )
 }
