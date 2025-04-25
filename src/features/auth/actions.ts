bf5d1f9af972c94b92fb2a8c7fffdd0bccb58ee2
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-04-25 19:45:12 -0300

    feat: auth actions

--- src/features/auth/actions.ts
+++ src/features/auth/actions.ts
@@ -1,53 +1,47 @@
-"use server"
-import { action, query, redirect, revalidate } from "@solidjs/router"
-import { SignInWithPasswordCredentials } from "@supabase/supabase-js"
-import { UserSignInSchema } from "./schemas"
-import { createClient } from "~/lib/supabase/server"
-import { getRequestEvent } from "solid-js/web"
-
-export const signIn = action(async (data: SignInWithPasswordCredentials) => {
-    const event = getRequestEvent()
-
-    if (!event) {
-        console.error("Got no event.")
-        return { error: new Error("Got no event.") }
-    }
-
-    const { supabase } = await createClient(event)
-    const { success, data: parsedData } = UserSignInSchema.safeParse(data)
-
-    if (!success || !data) {
-        return { error: new Error("Error parsing form data.") }
-    }
-
-    const { error } = await supabase.auth.signInWithPassword(parsedData)
-
-    if (error) {
-        return { error }
-    }
-
-    await revalidate("logged-user")
-
-    return {
-        success: true,
-    }
-})
+import { action, query, revalidate } from "@solidjs/router";
+import { SignInWithPasswordCredentials } from "@supabase/supabase-js";
+import { createClient } from "~/lib/supabase/server";
 
 export const getLoggedUser = query(async () => {
-    const event = getRequestEvent()
+    "use server"
 
-    if (!event) {
-        console.error("Got no event.")
-        return { error: new Error("Got no event.") }
+    const supabase = createClient()
+    const { data: { session }, error } = await supabase.auth.getSession()
+
+    if (error !== null) {
+        return { error: error.message }
+    } else if (session === null) {
+        return { error: "No user found." }
     }
 
-    const { supabase } = await createClient(event)
-    const { data, error } = await supabase.auth.getUser()
-
-    if (data === null || error !== null) {
-        throw redirect("/")
-    }
-
-    return data.user
+    return { user: session.user, error: null }
 
 }, "logged-user")
+
+// https://supabase.com/docs/guides/auth/signout#sign-out-and-scopes
+export const signOut = action(async (scope: "local" | "global" | "others" | undefined) => {
+    "use server"
+
+    const supabase = createClient()
+    const { error } = await supabase.auth.signOut({ scope })
+
+    if (error) {
+        console.error(error)
+        return { error: error.message }
+    }
+    
+    await revalidate("logged-user")
+    return { success: true }
+})
+
+export const signIn = action(async (data: SignInWithPasswordCredentials) => {
+    "use server"
+
+    const supabase = createClient()
+    const { error } = await supabase.auth.signInWithPassword(data)
+
+    if (error) return { error: error.message }
+
+    await revalidate("logged-user")
+    return { success: true }
+})
