cd192dc6e0f666e32dc8fd9967f974189011adb9
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-04-30 00:20:31 -0300

    feat: app header v1

--- src/features/header/components/app-header.tsx
+++ src/features/header/components/app-header.tsx
@@ -1,13 +1,15 @@
 import { SidebarTrigger } from '~/components/ui/sidebar'
 import { Notifications } from './notifications'
 import { notifications } from '../data'
+import { UserDropdown } from './user-dropdown'
 
 export function AppHeader() {
     return (
         <header class="flex h-16 shrink-0 items-center justify-between gap-2 border-b px-4">
             <SidebarTrigger class="-ml-1" />
-            <div>
+            <div class="flex gap-2 items-center">
                 <Notifications notifications={notifications} />
+                <UserDropdown />
             </div>
         </header>
     )
