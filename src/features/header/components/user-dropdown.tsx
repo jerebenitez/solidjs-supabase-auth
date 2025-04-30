9cc104d69eb012d9b9faab316c0e8e9631da8f95
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-04-30 00:20:07 -0300

    feat: added user dropdown

---
+++ src/features/header/components/user-dropdown.tsx
@@ -0,0 +1,47 @@
+import { createAsync } from '@solidjs/router'
+import { User } from '@supabase/auth-js'
+import LogOut from 'lucide-solid/icons/log-out'
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar'
+import { Button } from '~/components/ui/button'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from '~/components/ui/dropdown-menu'
+import { getLoggedUser, signOut } from '~/features/auth/actions'
+
+export function UserDropdown() {
+    const user = createAsync<User | null>(() => getLoggedUser())
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar>
+                    <AvatarImage src="" />
+                    <AvatarFallback>JB</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent class="w-64">
+                <DropdownMenuLabel>
+                    <p>Hello!</p>
+                    <span class="text-muted-foreground text-xs">
+                        {user()?.email}
+                    </span>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>My Profile</DropdownMenuItem>
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <form action={signOut.with('local')} method="post">
+                    <DropdownMenuItem as={Button} type="submit" variant="ghost" size="sm" class="w-full font-normal justify-start">
+                        <LogOut class="w-4" />
+                        Sign out
+                    </DropdownMenuItem>
+                </form>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -3,6 +3,6 @@ import { createBrowserClient } from "@supabase/ssr";
 export function createFrontendClient() {
     return createBrowserClient(
         import.meta.env.VITE_SUPABASE_URL,
-        import.meta.env.VITA_SUPABASE_KEY
+        import.meta.env.VITE_SUPABASE_KEY
     )
 }
